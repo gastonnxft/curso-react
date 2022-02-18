a791deea9f5de57b10be84e62408e799bd9885a7
Gaston Toledo <gastonnxft@gmail.com>
2022-02-18 00:10:30 -0300

    gaston-react

---
+++ gaston-react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container mt-3'>
+        <div className='jumbotron'>
+          <h1>mi proyecto react</h1>
+          <hr/>
+          <p>
+            mi primer clase asdasdasdasdasdasd
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
